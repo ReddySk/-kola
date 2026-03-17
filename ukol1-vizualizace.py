6f46156217601beb0a6b8661c3fb6ae8ba517fb9
ReddySk <reddy@macejka.eu>
2026-03-17 20:14:47 +0100

    Pridaný seaborn

--- ukol1-vizualizace.py
+++ ukol1-vizualizace.py
@@ -7,6 +7,9 @@ source_file = "vystup.txt"
 
 dataset = pd.read_csv(source_file)
 
+dataset['date_time'] = pd.to_datetime(dataset['date_time'])
+dataset['month'] = dataset['date_time'].dt.month
+
 fig = px.line(
     dataset,
     x="date_time",
@@ -15,3 +18,16 @@ fig = px.line(
     title="Mountains temperature over time in Q1 2026",
 )
 fig.show()
+
+
+plt.figure(figsize=(8, 6))
+glue = dataset.pivot_table(index="elevation", columns="month", values="temperature", aggfunc="mean")
+sns.heatmap(glue, annot=True, cmap="coolwarm")
+plt.title("Mountains temperature over time in Q1 2026")
+#plt.show()
+
+
+plt.figure(figsize=(8, 6))
+sns.scatterplot(data=dataset, x="date_time", y="temperature", hue="elevation", palette="Spectral")
+plt.title("Mountains temperature over time in Q1 2026")
+plt.show()
